761b9895f987502cc3f38ceceb861232a053adb1
Linus Aspelin <linus.aspelin@gmail.com>
2024-02-02 09:22:14 +0100

    Update day-22.py, found uncommitted changes 2024

--- day-22.py
+++ day-22.py
@@ -1,27 +1,37 @@
+import time
 import re
 test_file_1 = 'data/test/day-22.1.txt'
 test_file_2 = 'data/test/day-22.2.txt'
 data_file = 'data/day-22.txt'
 
 
+def progress(total, length=20, *, title='', sep=': '):
+    if title:
+        title += sep
+    start = time.perf_counter()
 
-def size(item):
-    x1, x2, y1, y2, z1, z2 = item
-    return (x2-x1+1) * (y2-y1+1) * (z2-z1+1)
+    def inner(done):
+        elapsed = time.perf_counter() - start
+        left = "#" * round((length * done) / total)
+        right = " " * (length - len(left))
+        bar = f'[{left}{right}]'
+        print(f'\33[1A\r{title}{bar} - {elapsed:.3f}s')
+
+    print()
+    inner(0)
+    return inner
 
 
-def overlaps(target, item):
-    def overlap(a1, a2, b1, b2):
-        return max(min(a2, b2) + 1 - max(a1, b1), 0)
-
-    tx1, tx2, ty1, ty2, tz1, tz2 = target
-    ix1, ix2, iy1, iy2, iz1, iz2 = item
-
-    if x := overlap(tx1, tx2, ix1, ix2):
-        if y := overlap(ty1, ty2, iy1, iy2):
-            z = overlap(tz1, tz2, iz1, iz2)
-            return x * y * z
-    return 0
+def overlaps(target):
+    def inner(item):
+        _target = [*target]
+        product = 1
+        while _target and product:
+            t1, t2, *_target = _target
+            i1, i2, *item = item
+            product *= max(min(t2, i2) + 1 - max(t1, i1), 0)
+        return product
+    return inner
 
 
 def cut_off(target, item, start, end):
@@ -44,13 +54,13 @@ def cut(target, item):
     target = list(target)
 
     x1, x2, y1, y2, z1, z2 = [0, 1, 2, 3, 4, 5]
-    if overlaps(target, item):
+    if overlaps(target)(item):
         parts = []
         target, *parts[0:] = cut_off(target, item, x1, x2)
         target, *parts[2:] = cut_off(target, item, y1, y2)
         target, *parts[4:] = cut_off(target, item, z1, z2)
-
         return list(filter(lambda x: x, parts))
+
     return [target]
 
 
@@ -58,29 +68,33 @@ def process(file):
     with open(file) as f:
         lines = f.read().splitlines()
 
-    seen = []
-    for line in lines:
+    seen = set()
+    length = len(lines)
+    bar = progress(length, title=file)
+
+    while lines:
+        line = lines.pop(0)
         action, line = line.split(' ')
         ranges = tuple(map(int, re.findall(r'-?\d+', line)))
 
-        processing = [(action == 'on', ranges)]
+        processing = {(action == 'on', ranges)}
         while processing:
             on, item = processing.pop()
-            
+            run_for = overlaps(item)
             for target in seen:
-                if overlaps(target, item):
+                if run_for(target):
                     seen.remove(target)
-                    for x in cut(target, item):
-                        processing.append((True, x))
-                    processing.append((on, item))
+                    seen |= set(map(tuple, cut(target, item)))
+                    processing.add((on, item))
                     break
             else:
                 if on:
-                    seen.append(item)
+                    seen.add(item)
+        bar(length - len(lines))
 
     init_proc_area = (-50, 50, -50, 50, -50, 50)
-    part_1 = sum(map(lambda x: overlaps(init_proc_area, x), seen))
-    part_2 = sum(map(size, seen))
+    part_1 = sum(map(overlaps(init_proc_area), seen))
+    part_2 = sum(map(lambda x: overlaps(x)(x), seen))
 
     return [part_1, part_2]
 
